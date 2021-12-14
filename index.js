ebdf34ec3bfb43c554d0f19977f5e4737fae00a1
Arvind <arbindudebnath59985@gmail.com>
2021-12-14 10:54:57 +0600

    set up mongodb and get data for gallery image

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient } = require('mongodb')
+
+
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 8000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tv1bc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run(){
+    try{
+        await client.connect()
+
+        const database = client.db('spicy-restaurant')
+        const foodImageCollection = database.collection('food_image')
+
+        app.get('/gallery', async(req, res) => {
+            const cursor = await foodImageCollection.find({})
+            const foodImage = await cursor.toArray()
+            res.send(foodImage)
+        })
+    }
+    finally{
+        // await client.close()
+    }
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('Server Running...')
+})
+
+// Listing port 
+
+app.listen(port, () => {
+    console.log('Server is running at', port)
+})
